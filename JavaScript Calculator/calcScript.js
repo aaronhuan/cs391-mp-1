128e7b635ae7a2a74a2dde980415b91162e28bb9
aaronhuan <aaronhuang0000@gmail.com>
2025-01-31 22:23:03 -0500

    finished pages + basic calc - need media queries - need implement clear - need styling in calc

---
+++ JavaScript Calculator/calcScript.js
@@ -0,0 +1,72 @@
+function calc(op){
+    let fval = document.getElementById("first-number").value;
+    let fnum = Number(fval);
+    let sval = document.getElementById("second-number").value; 
+    let snum = Number(sval);
+    let result = 0;
+    switch (op) {
+        case '+':
+            result = add(fnum, snum);
+    
+            break;
+        case '-':
+            result =sub(fnum, snum);
+          
+          break;
+        case '*':
+            result =mul(fnum,snum);
+
+            break;
+
+        case '/':
+            result =div(fnum, snum);
+            break;
+
+        case '**':
+            result =pow(fnum, snum);
+
+            break;
+        default:
+
+          break;
+      }
+
+      document.getElementById("answer").innerHTML= String(result)
+
+      if (result <0){  //red when negative
+        document.getElementById("answer").style.color="red";
+      }
+
+}
+
+
+
+
+function add(first,second){
+    return first + second;
+}
+
+function sub(first,second){
+    return first - second;
+}
+
+function mul(first,second){
+    return first * second;
+}
+
+function div(first,second){
+    return first/second 
+}
+
+function pow(first,second){
+    let result = 1
+
+    for(i=0;i <second; i++ ){
+        result *=first;
+    }
+    return result;
+}
+
+// function clear(first,second){
+
+// }
